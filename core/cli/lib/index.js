2ba238896fd703be8f3a9e7a134e7c80e5ed0148
paranoidyang <paranoidyang@163.com>
2023-03-28 22:30:01 +0800

    import-local的应用

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -3,5 +3,5 @@
 module.exports = core;
 
 function core() {
-  return 'Hello from core';
+  console.log('exec core')
 }
--- core/core/bin/index.js
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env node
-
-const utils = require('@yang-cli/utils')
-utils()
-console.log('hello, yang-cli!')
