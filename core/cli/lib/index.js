3e925cdcb41b6369fac223d0c206266ee7606b94
paranoidyang <paranoidyang@163.com>
2023-03-31 00:00:44 +0800

    添加最低 Node 版本检查功能

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -7,11 +7,29 @@ module.exports = core;
 // .json -> JSON.parse
 // .node -> c++
 // 其他任何文件都会被当做.js文件来解析，如果里面包含js代码就会被正常解析出来，所以也是可以获取.txt文件的， .txt -> .js
-const pkg = require('../package.json')
+const semver = require('semver')
+const colors = require('colors/safe')
 const log = require('@yang-cli/log')
+const pkg = require('../package.json')
+const constant = require('./const')
 
 function core() {
-  checkPkgVersion()
+  try {
+    checkPkgVersion()
+    checkNodeVersion()
+  }catch(e) {
+    log.error(e.message)
+  }
+}
+
+function checkNodeVersion() {
+  // 第一步，获取当前版本号
+  const currentVersion = process.version
+  // 第二步，比对最低版本号
+  const lowestVersion = constant.LOWEST_NODE_VERSION
+  if(!semver.gte(currentVersion, lowestVersion)) {
+    throw new Error(colors.red(`yang-cli 需要安装 v${lowestVersion} 以上版本的 Node.js`))
+  }
 }
 
 function checkPkgVersion() {
