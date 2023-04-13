17d8f01ef965637d0ea52b11961dee0941a8d10d
paranoidyang <paranoidyang@163.com>
2023-04-14 00:00:25 +0800

    脚手架初始化、全局参数注册

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -13,11 +13,14 @@ const colors = require('colors/safe')
 const userHome = require('user-home')
 // todo: 目前装的4.x版本的，最新版本的require会报错，后续看看解决方案
 const pathExists = require('path-exists').sync
+const commander = require('commander')
 const log = require('@yang-cli/log')
 const pkg = require('../package.json')
 const constant = require('./const')
 
 let args
+
+const program = new commander.Command()
 
 async function core() {
   try {
@@ -25,11 +28,49 @@ async function core() {
     checkNodeVersion()
     checkRoot()
     checkUserHome()
-    checkInputArgs()
+    // checkInputArgs()
     checkEnv()
     await checkGlobalUpdate()
+    registerCommand()
   }catch(e) {
     log.error(e.message)
+  }
+}
+
+// 利用commander注册命令
+function registerCommand() {
+  program
+    .name(Object.keys(pkg.bin)[0])
+    .usage('<command> [options]')
+    .version(pkg.version)
+    .option('-d, --debug', '是否开启调试模式', false)
+
+  // 开启debug模式
+  program.on('option:debug', function() {
+    if(program.opts().debug) {
+      process.env.LOG_LEVEL = 'verbose'
+    }else {
+      process.env.LOG_LEVEL = 'info'
+    }
+    log.level = process.env.LOG_LEVEL
+    // log.verbose('test')
+  })
+
+  // 对未知命令的监听
+  program.on('command:*', function(obj) {
+    const availableCommands = program.commands.map(cmd => cmd.name())
+    console.log(colors.red('未知的命令：' + obj[0]))
+    if(availableCommands.length > 0) {
+      console.log(colors.red('可用的命令：' + availableCommands.join(',')))
+    }
+  })
+
+
+  program.parse(process.argv)
+
+  if(program.args && program.args.length < 1) {
+    program.outputHelp()
+    console.log()
   }
 }
 
@@ -41,14 +82,11 @@ async function checkGlobalUpdate() {
   // 2、调用 npm API，获取所有版本号
   const { getNpmSemverVersion } = require('@yang-cli/get-npm-info')
   const lastVersion = await getNpmSemverVersion(currentVersion, npmName)
-  console.log(lastVersion)
+  // console.log(lastVersion)
   if(lastVersion && semver.gt(lastVersion, currentVersion)) {
     log.warn('更新提示', colors.yellow(`请手动更新 ${npmName}，当前版本：${currentVersion}，最新版本：${lastVersion}
     更新命令：npm install -g ${npmName}`))
   }
-  // 3、提取所有版本号，比对哪些版本是大于当前版本号
-
-  // 4、获取最新版本号，提升用户更新到该版本
 }
 
 // 检查环境变量
@@ -119,6 +157,6 @@ function checkNodeVersion() {
 }
 
 function checkPkgVersion() {
-  console.log(pkg.version)
-  log.notice('cli', pkg.version)
+  // console.log(pkg.version)
+  log.info('cli', pkg.version)
 }
